5c1e25b4fd44c16faba775f758b9114cf859d6c2
elizabethwangarinjuguna <wangarinjuguna106@gmail.com>
2025-07-02 17:55:09 +0300

    add js

---
+++ conditions.js
@@ -0,0 +1,25 @@
+let age=12
+if(age>=18){
+  console.log("you are an adult")
+}else{
+  console.log("you are a minor")
+}
+let marks=45
+if(marks>=50){
+  console.log("you passed the exam")
+}else{
+  console.log("you failed")
+}
+
+let grade=85
+if(grade>=90){
+  console.log("scored an A")
+}else if(grade>=80){
+  console.log("scored a B")
+}else if(grade>=70){
+  console.log("scored a C")
+}else if(grade>=60){
+  console.log("scored a D")
+}else{
+  console.log("you retake")
+}
---
+++ index.js
@@ -0,0 +1,32 @@
+// string
+let name="Elizabeth"
+console.log(name)
+// number
+let age=30
+console.log(age)
+
+const ID=234789
+console.log(ID)
+
+let LastName="Jubert"
+console.log(LastName)
+// Boolean true/false
+let IsAdult=true
+console.log(IsAdult)
+// undefiined a variable declared and not given a value
+let results;
+console.log(results)
+// objects- a key value pair
+let user={
+  name:"john",
+  age:30,
+  occupation:"nurse"
+}
+console.log(user)
+// an array-a list of values
+let fruits=["mango", "apple", "guava"]
+console.log(fruits)
+
+let marks=[30,40,50]
+console.log(marks)
+console.log(typeof LastName)
